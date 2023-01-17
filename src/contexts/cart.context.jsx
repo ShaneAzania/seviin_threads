1bbefbcdf8b99aae9761d3626fcc83bfdb2bfb69
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2023-01-17 12:34:27 -0500

    Replaced useEffect with updater function

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, /*useState,*/ useEffect, useReducer } from "react";
+import { createContext, useReducer } from "react";
 import {} from "../utils/firebase/firebase.utils";
 
 // the actual value you want to access
@@ -19,6 +19,7 @@ export const CART_ACTION_TYPES = {
 	SET_CART_ITEMS: "SET_CART_ITEMS",
 	SET_CART_COUNT: "SET_CART_COUNT",
 	SET_CART_TOTAL: "SET_CART_TOTAL",
+	SET_CART_COUNT_AND_TOTAL: "SET_CART_COUNT_AND_TOTAL",
 };
 const cartReducer = (state, action) => {
 	const { type, payload } = action;
@@ -27,24 +28,15 @@ const cartReducer = (state, action) => {
 		case CART_ACTION_TYPES.SET_IS_CART_OPEN:
 			return {
 				...state,
-				isCartOpen: !state.isCartOpen,
+				isCartOpen: payload,
 			};
 		case CART_ACTION_TYPES.SET_CART_ITEMS:
 			return {
 				...state,
-				cartItems: payload,
+				cartItems: payload.updatedCartItemsArray,
+				cartTotal: payload.totalAccumulator,
+				cartCount: payload.countAccumulator,
 			};
-		case CART_ACTION_TYPES.SET_CART_COUNT:
-			return {
-				...state,
-				cartCount: payload,
-			};
-		case CART_ACTION_TYPES.SET_CART_TOTAL:
-			return {
-				...state,
-				cartTotal: payload,
-			};
-
 		default:
 			throw new Error(`wrong action type: ${type}`);
 	}
@@ -60,71 +52,81 @@ const INITIAL_STATE = {
 export const CartProvider = ({ children }) => {
 	const [{ isCartOpen, cartItems, cartCount, cartTotal }, dispatch] = useReducer(cartReducer, INITIAL_STATE);
 
-	useEffect(() => {
+	const updateCartCountAndTotal = (theCartItems) => {
 		var countAccumulator = 0,
 			totalAccumulator = 0;
-		cartItems.map(({ quantity, price }) => {
+		theCartItems.map(({ quantity, price }) => {
 			countAccumulator += quantity;
 			totalAccumulator += price * quantity;
 			return 0;
 		});
 
-		dispatch({ type: CART_ACTION_TYPES.SET_CART_COUNT, payload: countAccumulator });
-		dispatch({ type: CART_ACTION_TYPES.SET_CART_TOTAL, payload: totalAccumulator });
-	}, [cartItems]);
+		return { totalAccumulator, countAccumulator };
+	};
 
 	const set_isCartOpen = () => {
-		dispatch({ type: CART_ACTION_TYPES.SET_IS_CART_OPEN });
-	};
-	const addItemToCart = (itemToAdd) => {
-		// If item is already in 'cartItems', increase the quantity value, else add 'itemToAdd'.
-		var updatedCartItemsArray = [];
-		// check if itemToAdd has a valid id in the cartItems[] already
-		const itemToAddPresent = cartItems.find((item) => item.id === itemToAdd.id);
+			dispatch({ type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: !isCartOpen });
+		},
+		addItemToCart = (itemToAdd) => {
+			// If item is already in 'cartItems', increase the quantity value, else add 'itemToAdd'.
+			var updatedCartItemsArray = [];
+			// check if itemToAdd has a valid id in the cartItems[] already
+			const itemToAddPresent = cartItems.find((item) => item.id === itemToAdd.id);
 
-		// if item is already present, increase it's quantity by 1
-		if (itemToAddPresent) {
-			updatedCartItemsArray = cartItems.map((item) =>
-				item.id === itemToAdd.id
-					? //if itemToAdd matches current item in cart, increase the existing item quantity by 1
-					  { ...item, quantity: item.quantity + 1 }
-					: //if current item does not match, just add current item to the new cart array
-					  item
-			);
-		} // if item is NOT already present, add quantity value to item, then add item to updatedCartItemsArray
-		else {
-			updatedCartItemsArray = [...cartItems, { ...itemToAdd, quantity: 1 }];
-		}
-
-		dispatch({ type: CART_ACTION_TYPES.SET_CART_ITEMS, payload: updatedCartItemsArray });
-	};
-	const subtractItemFromCart = (itemToSubtract) => {
-		var updatedCartItemsArray = [];
-
-		// check if item is in cartItems
-		const itemToSubtractPresent = cartItems.find((item) => item.id === itemToSubtract.id);
-
-		// if item present, decrease quantity, else don't do anything
-		if (itemToSubtractPresent) {
-			updatedCartItemsArray = cartItems.map((item) => {
-				if (item.id === itemToSubtractPresent.id) {
-					// check quantity
-					return { ...item, quantity: item.quantity - 1 };
-				} else {
-					return item;
-				}
+			// if item is already present, increase it's quantity by 1
+			if (itemToAddPresent) {
+				updatedCartItemsArray = cartItems.map((item) =>
+					item.id === itemToAdd.id
+						? //if itemToAdd matches current item in cart, increase the existing item quantity by 1
+						  { ...item, quantity: item.quantity + 1 }
+						: //if current item does not match, just add current item to the new cart array
+						  item
+				);
+			} // if item is NOT already present, add quantity value to item, then add item to updatedCartItemsArray
+			else {
+				updatedCartItemsArray = [...cartItems, { ...itemToAdd, quantity: 1 }];
+			}
+			const { totalAccumulator, countAccumulator } = updateCartCountAndTotal(updatedCartItemsArray);
+			dispatch({
+				type: CART_ACTION_TYPES.SET_CART_ITEMS,
+				payload: { updatedCartItemsArray, totalAccumulator, countAccumulator },
 			});
-			// check if quantity reaches 0, don't add to updated cart array)
-			updatedCartItemsArray = updatedCartItemsArray.filter((item) => item.quantity > 0);
+		},
+		subtractItemFromCart = (itemToSubtract) => {
+			var updatedCartItemsArray = [];
 
-			dispatch({ type: CART_ACTION_TYPES.SET_CART_ITEMS, payload: updatedCartItemsArray });
-		}
-	};
-	const deleteFromCart = (itemToDelete) => {
-		const updatedCartItemsArray = cartItems.filter((item) => item.id !== itemToDelete.id);
+			// check if item is in cartItems
+			const itemToSubtractPresent = cartItems.find((item) => item.id === itemToSubtract.id);
 
-		dispatch({ type: CART_ACTION_TYPES.SET_CART_ITEMS, payload: updatedCartItemsArray });
-	};
+			// if item present, decrease quantity, else don't do anything
+			if (itemToSubtractPresent) {
+				updatedCartItemsArray = cartItems.map((item) => {
+					if (item.id === itemToSubtractPresent.id) {
+						// check quantity
+						return { ...item, quantity: item.quantity - 1 };
+					} else {
+						return item;
+					}
+				});
+				// check if quantity reaches 0, don't add to updated cart array)
+				updatedCartItemsArray = updatedCartItemsArray.filter((item) => item.quantity > 0);
+
+				const { totalAccumulator, countAccumulator } = updateCartCountAndTotal(updatedCartItemsArray);
+				dispatch({
+					type: CART_ACTION_TYPES.SET_CART_ITEMS,
+					payload: { updatedCartItemsArray, totalAccumulator, countAccumulator },
+				});
+			}
+		},
+		deleteFromCart = (itemToDelete) => {
+			const updatedCartItemsArray = cartItems.filter((item) => item.id !== itemToDelete.id);
+
+			const { totalAccumulator, countAccumulator } = updateCartCountAndTotal(updatedCartItemsArray);
+			dispatch({
+				type: CART_ACTION_TYPES.SET_CART_ITEMS,
+				payload: { updatedCartItemsArray, totalAccumulator, countAccumulator },
+			});
+		};
 
 	const value = {
 		isCartOpen,
