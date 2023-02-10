c8c264a9d4eca264a657837a962d3d314c117369
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2023-02-10 14:49:10 -0500

    updated categories selector with reselect

--- src/redux-store/categories/categories.selector.js
+++ src/redux-store/categories/categories.selector.js
@@ -1,1 +1,5 @@
-export const categories_selector = (state) => state.categories;
+import { createSelector } from "reselect";
+
+const categories_reducer = (state) => state.categories;
+
+export const categories_selector = createSelector([categories_reducer], (categories) => categories);
